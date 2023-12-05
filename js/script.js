f8aea6bc420a5bad0512e2ddb0931221f60e26db
stephanieparra <stephanieparrastudio@gmail.com>
2023-12-05 15:01:52 -0800

    Added function for guest count and hiding button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 // invite button
 const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
+
 addGuestButton.addEventListener("click", function () {
   const guest = guestInput.value;
   //console.log(guest);
   if (guest !== "") {
     addToList(guest);
+    updateGuestCount();
     clearInput();
   }
 });
@@ -19,13 +31,14 @@ const addToList = function (guest) {
   guestList.append(listItem);
 };
 
-// label for the invite button
-const guestInputLabel = document.querySelector(".add-guest label");
-// text input box
-const guestInput = document.querySelector(".add-guest input");
-// unordered list (not yet visible)
-const guestList = document.querySelector(".guest-list");
-// span class for number of guests attending
-const guestCount = document.querySelector(".attendance");
-// alert when guest list is full (not yet visible)
-const guestFull = document.querySelector(".alert");
+const updateGuestCount = function () {
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
