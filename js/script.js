08b94c6e19b8ba8412475f1eae670eb701fb3ac0
stephanieparra <stephanieparrastudio@gmail.com>
2023-12-05 13:11:03 -0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// invite button
+const addGuestButton = document.querySelector(".invite");
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  //console.log(guest);
+  if (guest !== "") {
+    let listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+  }
+});
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
